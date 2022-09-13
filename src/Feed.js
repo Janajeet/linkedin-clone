f91173f8ea64e30533e6a1515e85acd75ff5833f
Janajeet <janajeetbose@gmail.com>
2022-09-13 18:11:54 +0530

    We can post now. YAY!

--- src/Feed.js
+++ src/Feed.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Feed.css'
 import CreateIcon from '@mui/icons-material/Create';
 import ImageIcon from '@mui/icons-material/Image';
@@ -8,13 +8,40 @@ import YouTubeIcon from '@mui/icons-material/YouTube';
 import EventIcon from '@mui/icons-material/Event';
 import ArticleIcon from '@mui/icons-material/Article';
 import Post from './Post';
+import { db } from './firebase';
+// import { set } from 'immer/dist/internal';
+// import firebase from 'firebase';
+import firebase from 'firebase/compat/app';
+
 
 function Feed() {
-    
+    const [input, setInput] = useState('');
     const [posts, setPosts] = useState([]);
+    
+    useEffect(() => {
+        db.collection("posts").orderBy("timestamp","desc")
+        .onSnapshot((snapshot) => 
+            setPosts(
+                snapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    data: doc.data(),
+                }))
+
+        )
+        )
+    }, []);
 
     const sendPost = (e) => {
         e.preventDefault();
+
+        db.collection('posts').add({
+            name:"Janajeet Bose",
+            description:"this is a test",
+            message: input,
+            photoUrl:"",
+            timestamp:firebase.firestore.FieldValue.serverTimestamp(),
+        });
+        setInput("")
     };
 
   return( <div className='feed'>
@@ -22,7 +49,10 @@ function Feed() {
             <div className='feed__input'>
                 <CreateIcon />
                 <form>
-                    <input type="text" />
+                    <input 
+                        value={ input } 
+                        onChange = {(e) => setInput(e.target.value)}
+                        type = "text"/>
                     <button onClick={sendPost} type='submit'>Send</button>
                 </form>
             </div>
@@ -33,13 +63,19 @@ function Feed() {
                 <InputOptions Icon={ArticleIcon} title ="Write article" color ="#e16745"/>
             </div>
         </div>
-        {posts.map((post) => (
-            <Post />
+        {posts.map(({id, data:{ name, description, message, photoUrl}}) => (
+            <Post 
+            key={id}
+            name={name}
+            description = {description}
+            message = {message}
+            photoUrl = {photoUrl}
+            />
         ))}
-        <Post 
+        {/* <Post 
         name="Janajeet Bose"
         description="This is a test"
-        message="This works" />
+        message="This works" /> */}
     </div>
   );
 }
--- src/Firebase.js
+++ src/Firebase.js
@@ -1,4 +1,8 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyDELqcYpeAdT14j122PrbULr4xfAFrfNKw",
     authDomain: "linkedin-clone-b12dc.firebaseapp.com",
@@ -8,4 +12,10 @@ const firebaseConfig = {
     appId: "1:385505323676:web:638a9be0a4bca956425b59",
     measurementId: "G-JK8656BTH3"
   };
-  
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export{ db,auth };
